237831589ed774892a0368957728e7ab218b3e2c
Rosky <jorge.blanco.gil.00@gmail.com>
2021-03-08 23:40:34 +0100

    tema 7 ejer comerciales 2 / 2

--- src/ejers_prog/tema7/tanda1/ejer3/GestionComercial2.java
+++ src/ejers_prog/tema7/tanda1/ejer3/GestionComercial2.java
@@ -2,18 +2,16 @@ package ejers_prog.tema7.tanda1.ejer3;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-public class GestionComercial {
-
+public class GestionComercial2 {
 	private String nomFich;
 
-	public GestionComercial(String nomFich) {
+	public GestionComercial2(String nomFich) {
 		super();
 		this.nomFich = nomFich;
 	}
@@ -23,11 +21,7 @@ public class GestionComercial {
 		FileOutputStream fos = new FileOutputStream(f);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-		for (Comercial comercial : arr) {
-			oos.writeObject(comercial);
-		}
-
-		oos.writeObject(null);
+		oos.writeObject(arr);
 		oos.close();
 
 	}
@@ -36,85 +30,76 @@ public class GestionComercial {
 		FileInputStream fis = new FileInputStream("files/" + nomFich);
 		ObjectInputStream ois = new ObjectInputStream(fis);
 
-		boolean cont = true;
-		while (cont) {
-			Comercial obj = (Comercial) ois.readObject();
-			if (obj != null)
-				obj.ver();
-			else
-				cont = false;
-		}
+		ArrayList<Comercial> arr = (ArrayList<Comercial>) ois.readObject();
 
 		ois.close();
+		for (Comercial comercial : arr) {
+			comercial.ver();
+		}
+
 	}
 
 	public Comercial buscaComercial(String nomComer) throws IOException, ClassNotFoundException {
 		FileInputStream fis = new FileInputStream("files/" + nomFich);
 		ObjectInputStream ois = new ObjectInputStream(fis);
-		
-		boolean available = true;
-		while(available) {
-			Comercial obj = (Comercial) ois.readObject();
-			if(obj != null) {
-				 if( obj.getNombre().equals(nomComer)) return obj;
-			}
-			else available = false;
-		}
-		
+
+		ArrayList<Comercial> arr = (ArrayList<Comercial>) ois.readObject();
+
 		ois.close();
+
+		for (Comercial comercial : arr) {
+			if (comercial.getNombre() == nomComer)
+				return comercial;
+		}
+
 		return null;
-		
-		
+
 	}
-	
+
 	public void generaFichMoviles(String nomFich) throws IOException, ClassNotFoundException {
 		FileOutputStream fos = new FileOutputStream("files/" + nomFich);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		
+
 		FileInputStream fis = new FileInputStream("files/" + this.nomFich);
 		ObjectInputStream ois = new ObjectInputStream(fis);
-		
-		boolean available = true;
-		while(available) {
-			Comercial obj = (Comercial) ois.readObject();
-			if(obj != null) {
-				if(obj.getTelf() != null) {
-					obj.getTelf().cargar(10);
-					oos.writeObject(obj.getTelf());
-				}
-			} else available = false;
-		}
-		
-		oos.writeObject(null);
+
+		ArrayList<Comercial> arr = (ArrayList<Comercial>) ois.readObject();
+
 		ois.close();
+
+		for (Comercial comercial : arr) {
+			if (comercial != null) {
+				if (comercial.getTelf() != null) {
+					comercial.getTelf().cargar(10);
+					oos.writeObject(comercial.getTelf());
+				}
+			}
+		}
+
 		oos.close();
-		
+
 	}
-	
+
 	public void trabajarTodos() throws IOException, ClassNotFoundException {
 		FileInputStream fis = new FileInputStream("files/" + nomFich);
 		ObjectInputStream ois = new ObjectInputStream(fis);
-		
-		ArrayList<Comercial> arr = new ArrayList<Comercial>();
-		
-		boolean available = true;
-		while(available) {
-			Comercial obj = (Comercial) ois.readObject();
-			if(obj != null) {
-				obj.trabajar();
-				arr.add(obj);
-			} else available = false;
+
+		ArrayList<Comercial> arr =  (ArrayList<Comercial>) ois.readObject();
+	
+		for (Comercial comercial : arr) {
+			comercial.trabajar();
 		}
 		
+		ois.close();
+
+
 		FileOutputStream fos = new FileOutputStream("files/" + nomFich);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		
-		for (Comercial comercial : arr) {
-			oos.writeObject(comercial);
-		}
-		oos.writeObject(null);
+
+
+		oos.writeObject(arr);
 		oos.close();
-		
+
 	}
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -130,14 +115,14 @@ public class GestionComercial {
 		a.buscaComercial("R2D2").ver();
 		System.out.println(a.buscaComercial("ddddd"));
 		a.generaFichMoviles("moviles.obj");
-		
+
 		FileInputStream fis = new FileInputStream("files/" + "moviles.obj");
 		ObjectInputStream ois = new ObjectInputStream(fis);
-		
+
 		boolean cont = true;
 		while (cont) {
 			TelefonoMovil obj = (TelefonoMovil) ois.readObject();
-			
+
 			if (obj != null)
 				obj.ver();
 			else
@@ -145,9 +130,8 @@ public class GestionComercial {
 		}
 
 		ois.close();
-		
+
 		a.trabajarTodos();
 		a.verComerciales();
 	}
-
 }
