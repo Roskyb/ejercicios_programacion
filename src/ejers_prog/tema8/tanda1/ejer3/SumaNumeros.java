856d6b9d81d8bb89f857facfd83a6a86fac64be2
Rosky <jorge.blanco.gil.00@gmail.com>
2021-03-23 11:00:22 +0100

    tema 8 tanda 1 ejer3 fix

--- src/ejers_prog/tema8/tanda1/ejer3/SumaNumeros.java
+++ src/ejers_prog/tema8/tanda1/ejer3/SumaNumeros.java
@@ -4,16 +4,19 @@ public class SumaNumeros {
 
 	public static void main(String[] args) {
 		int suma = 0;
+		int cont = 0;
 		for (String num : args) {
 			
 			try {
 				suma += Integer.parseInt(num);
 			}catch(NumberFormatException e) {
 				System.out.println(num + " no se puede sumar");
+				cont++;
 			}
 		}
 		
-		System.out.println(suma);
+		System.out.println("La suma total es " + suma);
+		System.out.println("No se han podido sumar" + cont + " números");
 		
 	}
 	
