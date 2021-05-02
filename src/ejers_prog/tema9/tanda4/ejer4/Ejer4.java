2c88d6ae382dce899851a0c613a9f5ee985c3d3e
Rosky <jorge.blanco.gil.00@gmail.com>
2021-05-02 19:27:25 +0200

    fixes

--- src/ejers_prog/tema9/tanda4/ejer4/Ejer4.java
+++ src/ejers_prog/tema9/tanda4/ejer4/Ejer4.java
@@ -1,27 +1,20 @@
 package ejers_prog.tema9.tanda4.ejer4;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
+
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.Iterator;
+
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
